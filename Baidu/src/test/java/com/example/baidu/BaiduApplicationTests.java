40d9093d73f582b9a67fb52765045cf2a373ce1e
lxj <1873579697@qq.com>
2021-10-25 22:14:27 +0800

    ok

---
+++ Baidu/src/test/java/com/example/baidu/BaiduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baidu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
