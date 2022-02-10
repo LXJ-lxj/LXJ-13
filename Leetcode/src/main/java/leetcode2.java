6254ac8ac1ce6314873e69252a0187e5bf950722
lxj <1873579697@qq.com>
2022-02-10 20:46:07 +0800

    leetcode

--- Leetcode/src/main/java/leetcode2.java
+++ Leetcode/src/main/java/leetcode2.java
@@ -1,14 +1,31 @@
-public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
-        long s = 1, e = (long)n+1, mid = 0;
-        while (s < e) {
-            mid = (s + e)/2;
-            if (isBadVersion((int)mid)) {
-                e = mid;
-            } else {
-                s = mid + 1;
+public class leetcode2 {
+    public static void main(String[] args) {
+        int [] nums= {1,3,5,6};
+        find(nums,5);
+    }
+    private static String find(int[] arr,int num) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right){
+            if(num < arr[left]){   //未找到之应该插入到最开始位置
+                return "无该元素，应该被插入到："+0+"号位置";
+            }
+            if(num > arr[right]){   //未找到之应该插入到最后位置
+                return "无该元素，应该被插入到："+arr.length+"号位置";
+            }
+
+            int mid = (right - left + 1)/2 + left;
+            if(arr[mid] == num){    //找到
+                return "该元素下标为："+mid;
+            }else if(arr[mid] > num){
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+            if(arr[mid] < num && arr[mid+1] > num){   //未找到之应该插入到中间某个节点(不用判断mid的合法性，第一个判断，就不会让mid > arr.length -1).
+                return "无该元素，应该被插入到："+(mid+1)+"号位置";
             }
         }
-        return (int)s;
+        return null;
     }
 }
