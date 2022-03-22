2c820342f236194da2ea592bd1759cc23e136de6
lxj <1873579697@qq.com>
2022-03-22 18:06:31 +0800

    leetcode

---
+++ Leetcode/src/main/java/leetcode13.java
@@ -0,0 +1,18 @@
+public class leetcode13 {
+    public static void main(String[] args) {
+        int x = 121;
+        System.out.println(isPalindrome(x));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        int cur = 0;
+        int num = x;
+        while(num != 0) {
+            cur = cur * 10 + num % 10;
+            num /= 10;
+        }
+        return cur == x;
+    }
+}
